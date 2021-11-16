b7cf8bae1a7e0d02edaabb109c8725fa04eecb82
dekken <philip.deegan@gmail.com>
2021-11-16 12:00:21 +0100

    bash else to avoid non-zero exit code

--- mkn.sh
+++ mkn.sh
@@ -7,5 +7,5 @@ GIT_URL="https://github.com/dpilger26/NumCpp"
 HAS=1
 [ ! -d "$CWD/$DIR" ] && HAS=0 && git clone --depth 1 $GIT_URL -b $VERSION $DIR --recursive --shallow-submodules
 cd $CWD/$DIR
-[ $HAS -eq 1 ] && git pull --recurse-submodules origin $VERSION
+[ $HAS -eq 1 ] && git pull --recurse-submodules origin $VERSION || echo ""
 
